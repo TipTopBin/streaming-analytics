5eb88e5ceaa7a7287d27cedca223fab2510e826a
EC2 Default User <ec2-user@ip-172-31-11-178.ec2.internal>
2023-02-18 11:08:34 +0000

    update scripts

---
+++ utils/scripts/prepareCloud9IDE-slim.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -e
+
+download_and_verify () {
+  url=$1
+  checksum=$2
+  out_file=$3
+
+  curl --location --show-error --silent --output $out_file $url
+
+  echo "$checksum $out_file" > "$out_file.sha256"
+  sha256sum --check "$out_file.sha256"
+  
+  rm "$out_file.sha256"
+}
+
+
+cd /tmp/
+
+echo "==============================================="
+echo "  Install eksctl ......"
+echo "==============================================="
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+# 配置自动完成
+cat >> ~/.bashrc <<EOF
+. <(eksctl completion bash)
+alias e=eksctl
+complete -F __start_eksctl e
+EOF
+
+
+echo "==============================================="
+echo "  Install kubectl ......"
+echo "==============================================="
+# 安装 kubectl 并配置自动完成
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+cat >> ~/.bashrc <<EOF
+source <(kubectl completion bash)
+alias k=kubectl
+complete -F __start_kubectl k
+EOF
+source ~/.bashrc
+kubectl version --client
+# Enable some kubernetes aliases
+echo "alias kgn='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L node.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone'" | tee -a ~/.bashrc
+echo "alias kk='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L karpenter.sh/capacity-type -L node.kubernetes.io/instance-type -L topology.kubernetes.io/zone -L karpenter.sh/provisioner-name'" | tee -a ~/.bashrc
+echo "alias kgp='kubectl get po -o wide'" | tee -a ~/.bashrc
+
+
+echo "==============================================="
+echo "  Install helm ......"
+echo "==============================================="
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm version
+helm repo add stable https://charts.helm.sh/stable
+
+
+echo "==============================================="
+echo "  Install Flux CLI ......"
+echo "==============================================="
+curl -s https://fluxcd.io/install.sh | sudo bash
+flux --version
+
+
+echo "==============================================="
+echo "  More Aliases ......"
+echo "==============================================="
+cat >> ~/.bashrc <<EOF
+alias c=clear
+EOF
+source ~/.bashrc
+
+
+# 最后再执行一次 source
+echo "source .bashrc"
+shopt -s expand_aliases
+source ~/.bashrc
