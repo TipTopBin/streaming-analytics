22e9a0226b7a704fd3ef42f4ce850b4a94ac6e60
Robin Long <binlon@amazon.com>
2023-03-07 22:02:54 +0800

    update Tokenizer

--- code/benchmark/src/main/java/com/amazonaws/services/kinesisanalytics/Tokenizer.java
+++ code/benchmark/src/main/java/com/amazonaws/services/kinesisanalytics/Tokenizer.java
@@ -1,4 +1,8 @@
-public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+import org.apache.flink.api.common.functions.*;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
     @Override
     public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
